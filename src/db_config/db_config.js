50dfe78f4865778aab448074b90fbc69bf97a9d0
jamesweeba <65941501+jamesweeba@users.noreply.github.com>
2022-09-11 23:51:17 +0000

    Update db_config.js

--- src/db_config/db_config.js
+++ src/db_config/db_config.js
@@ -2,10 +2,10 @@
 module.exports = {
     postgresdb: {
       local: {
-        host: process.env.PG_HOST ||'ec2-52-0-203-66.compute-1.amazonaws.com',
-        user: process.env.PG_USER  ||'euqwewwhdngquo'||'postgres' ||"insyt",
-        password: process.env.PG_PASSWD  ||'5feb7daf3b0ea14cc03eafd72da08b69fbf7e23ca5907dcf45e6681ce4cdacd6'||"9XkaqkyTnldoQ",
-        database: process.env.PG_DBNAME   || 'dbm6qjjnlmj647',
+        host: process.env.PG_HOST ||'anything.amazonaws.com',
+        user: process.env.PG_USER  ||'postgres' ||,
+        password: process.env.PG_PASSWD  ||"9XkaqkyTnldoQ",
+        database: process.env.PG_DBNAME   || 'anything',
         port: process.env.PG_PORT || 5432,
         ssl: {
            rejectUnauthorized: false
@@ -19,15 +19,3 @@ module.exports = {
     }
   }
   
-  /*
-Host
-    ec2-52-0-203-66.compute-1.amazonaws.com
-Database
-    dbm6qjjnlmj647
-User
-    euqwewwhdngquo
-Port
-    5432
-Password
-    5feb7daf3b0ea14cc03eafd72da08b69fbf7e23ca5907dcf45e6681ce4cdacd6
-  */
