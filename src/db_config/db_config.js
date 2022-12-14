e35169bb4eb7f02fb58a0ef78d62edff852f513b
quaci <jamesweeba@gmail.com>
2022-12-14 12:05:29 +0000

    forgive me God

--- src/db_config/db_config.js
+++ src/db_config/db_config.js
@@ -2,17 +2,17 @@
   module.exports = {
     postgresdb: {
       local: {
-        host: process.env.PG_HOST ,
-        user: process.env.PG_USER  ,
-        password: process.env.PG_PASSWD ,
-        database: process.env.PG_DBNAME  ,
-        port: process.env.PG_PORT ,
+        host: process.env.PG_HOST ||'dpg-cecllp94rebeiecml2kg-a.oregon-postgres.render.com',
+        user: process.env.PG_USER  ||'enyeta',
+        password: process.env.PG_PASSWD  ||'7VP95TppNuDXj4CF8weLhMp2VCc4EuEo',
+        database: process.env.PG_DBNAME   || 'incident_db',
+        port: process.env.PG_PORT || 5432,
         ssl: {
            rejectUnauthorized: true
         },
-        connectionTimeoutMillis: process.env.PG_CONNECT_TIMEOUT ,
-        idleTimeoutMillis: process.env.PG_IDLE_TIMEOUT ,
-        max: process.env.PG_MAX_POOL 
+        connectionTimeoutMillis: process.env.PG_CONNECT_TIMEOUT || 7200,
+        idleTimeoutMillis: process.env.PG_IDLE_TIMEOUT || 7200,
+        max: process.env.PG_MAX_POOL || 400,
        
       }   
       
