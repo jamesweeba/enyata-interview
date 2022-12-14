c6fa12e60cabcf28663f91fe8ae4864a58868dd9
quaci <jamesweeba@gmail.com>
2022-12-14 11:34:15 +0000

    obvious

--- src/db_config/db_config.js
+++ src/db_config/db_config.js
@@ -1,22 +1,18 @@
 
-
-
-  // postgres://aspewvzp:0qyBzoB1yEhALYJr2Rw5AwOFf_kvtDVj@mel.db.elephantsql.com/aspewvzp 
-
   module.exports = {
     postgresdb: {
       local: {
-        host: process.env.PG_HOST ||'dpg-cecllp94rebeiecml2kg-a.oregon-postgres.render.com',
-        user: process.env.PG_USER  ||'enyeta'||'euqwewwhdngquo'||'postgres' ||"insyt",
-        password: process.env.PG_PASSWD  ||'7VP95TppNuDXj4CF8weLhMp2VCc4EuEo'||"9XkaqkyTnldoQ",
-        database: process.env.PG_DBNAME   || 'incident_db'||'dbm6qjjnlmj647',
-        port: process.env.PG_PORT || 5432,
+        host: process.env.PG_HOST ,
+        user: process.env.PG_USER  ,
+        password: process.env.PG_PASSWD ,
+        database: process.env.PG_DBNAME  ,
+        port: process.env.PG_PORT ,
         ssl: {
            rejectUnauthorized: true
         },
-        connectionTimeoutMillis: process.env.PG_CONNECT_TIMEOUT || 7200,
-        idleTimeoutMillis: process.env.PG_IDLE_TIMEOUT || 7200,
-        max: process.env.PG_MAX_POOL || 400,
+        connectionTimeoutMillis: process.env.PG_CONNECT_TIMEOUT ,
+        idleTimeoutMillis: process.env.PG_IDLE_TIMEOUT ,
+        max: process.env.PG_MAX_POOL ,
        
       }   
       
@@ -24,6 +20,5 @@
   }
 
 
-  //postgres://enyeta:7VP95TppNuDXj4CF8weLhMp2VCc4EuEo@dpg-cecllp94rebeiecml2kg-a.oregon-postgres.render.com/incident_db
   
   
